b848aa57993ad9936ec928bc92c1a333d92f3511
Bruno Tavares <bruno.exz@gmail.com>
2014-02-21 16:39:09 -0300

    Add function to create a user with default password if it does not exist

--- kickstart/recipes/kickstart/user.sh
+++ kickstart/recipes/kickstart/user.sh
@@ -1,3 +1,7 @@
+kickstart.user.create() {
+  kickstart.mute "id $1" || ( useradd -m -s /bin/bash -U -p `openssl passwd -1 $2` $1 )
+}
+
 kickstart.user.add_group() {
   kickstart.info "Adding $2 group to $1"
   grep -q $2 <(groups $1) || usermod -a -G $2 $1
