e6ba63f4709c6db52bab289b250d14a2e26fc048
Bruno Tavares <bruno.exz@gmail.com>
2014-02-21 15:17:22 -0300

    Remove yum support. I didn't test on it yet

--- kickstart/recipes/kickstart/package.sh
+++ kickstart/recipes/kickstart/package.sh
@@ -1,14 +1,12 @@
 kickstart.package.manager() {
   kickstart.command_exists apt-get && echo "apt-get" && return 0
-  kickstart.command_exists yum && echo "yum" && return 0
   kickstart.command_exists brew && echo "brew" && return 0
-  kickstart.info "kickstart supports apt-get, yum or brew" && exit 1
+  kickstart.info "kickstart supports apt-get or brew" && exit 1
 }
 
 kickstart.package.installed() {
   local pkg_manager=`kickstart.package.manager`
   [ $pkg_manager = 'apt-get' ] && dpkg -s $@ >/dev/null 2>&1 && return $?
-  [ $pkg_manager = 'yum' ] && rpm -qa | grep -q $@ && return $?
   [ $pkg_manager = 'brew' ] && ! $(brew info $@ | grep -q "Not installed") && return $?
 }
 
