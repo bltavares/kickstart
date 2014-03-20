d8ba5cbb5968a70252744d78f13cf280a15ed8a5
Bruno Tavares <bruno.exz@gmail.com>
2014-03-20 18:37:21 -0300

    package upgrade and update for pacman

--- kickstart/recipes/kickstart/package/update.sh
+++ kickstart/recipes/kickstart/package/update.sh
@@ -1,5 +1,19 @@
-
 kickstart.package.update() {
-  kickstart.mute `kickstart.package.manager` update -y
+  kickstart.package.update.`kickstart.package.manager`
 }
 
+kickstart.package.update.apt-get() {
+  kickstart.mute apt-get update -y
+}
+
+kickstart.package.update.brew() {
+  kickstart.mute brew update -y
+}
+
+kickstart.package.update.yum() {
+  kickstart.mute yum update -y
+}
+
+kickstart.package.update.pacman() {
+  kickstart.mute pacman -Sy --noconfirm
+}
--- kickstart/recipes/kickstart/package/upgrade.sh
+++ kickstart/recipes/kickstart/package/upgrade.sh
@@ -1,3 +1,19 @@
 kickstart.package.upgrade() {
-  kickstart.mute `kickstart.package.manager` upgrade -y
+  kickstart.package.upgrade.`kickstart.package.manager`
+}
+
+kickstart.package.upgrade.apt-get() {
+  kickstart.mute apt-get upgrade -y
+}
+
+kickstart.package.upgrade.brew() {
+  kickstart.mute brew upgrade -y
+}
+
+kickstart.package.upgrade.yum() {
+  kickstart.mute yum upgrade -y
+}
+
+kickstart.package.upgrade.pacman() {
+  kickstart.mute pacman -Suy --noconfirm
 }
