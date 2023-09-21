fe170047845a461c2fbe7856689189c1640124cc
Bruno Tavares <connect+github@bltavares.com>
2023-09-21 03:23:16 +0000

    Add opensuse detection to kickstart.os

--- kickstart/recipes/kickstart/os.sh
+++ kickstart/recipes/kickstart/os.sh
@@ -2,6 +2,7 @@ kickstart.os() {
   [ -f /etc/lsb-release ] && echo "Ubuntu"
   [ -f /etc/redhat-release ] && echo "RHEL"
   [ -f /etc/arch-release ] && echo "Arch"
+  [ -f /etc/os-release ] && kickstart.file.contains /etc/os-release suse && echo "Suse"
   uname -a | kickstart.stream.contains Darwin && echo "Mac"
 }
 
